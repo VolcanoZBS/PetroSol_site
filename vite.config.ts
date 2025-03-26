191a129bdea32a13047b5aae06245d9f696626ab
VolcanoZBS <stavinschi.denis@gmail.com>
2025-03-26 15:12:05 +0200

    changes so it would work on netlify and github

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/PetroSol_site/' : '/',
+  base: process.env.DEPLOY_TARGET === 'GH_PAGES' ? '/PetroSol_site/' : '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
